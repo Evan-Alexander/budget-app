6610a11c17d328ec5986e788627cc8ed3c135c44
jb <jasontbrown99@gmail.com>
2021-03-11 18:20:42 -0800

    add text filter

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = ({ amount, createdAt, description, note }) => {
+const ExpenseListItem = ({
+  dispatch,
+  amount,
+  createdAt,
+  description,
+  note,
+  id,
+}) => {
   return (
     <div>
       <p>{amount}</p>
       <p>{description}</p>
+      <button
+        onClick={() => {
+          dispatch(removeExpense({ id }));
+        }}
+      >
+        REMOVE
+      </button>
     </div>
   );
 };
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
