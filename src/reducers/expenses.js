0c4f895843a090e67925ab1feb78989d10e7f8fa
jb <jasontbrown99@gmail.com>
2021-03-11 16:37:11 -0800

    add redux to app

---
+++ src/reducers/expenses.js
@@ -0,0 +1,23 @@
+const expensesDefaultState = [];
+
+export default (state = expensesDefaultState, action) => {
+  switch (action.type) {
+    case "ADD_EXPENSE":
+      return [...state, action.expense];
+    case "REMOVE_EXPENSE":
+      return state.filter(({ id }) => id !== action.id);
+    case "EDIT_EXPENSE":
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            // return all of existing expense
+            ...expense,
+            // will override any changes to that expense
+            ...action.expense,
+          };
+        }
+      });
+    default:
+      return state;
+  }
+};
