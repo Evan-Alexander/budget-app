66decfd6bea74eade07c1c6a4de68b2f1b0c34ec
jb <jasontbrown99@gmail.com>
2021-03-25 14:16:15 -0700

    add public route

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,18 +8,13 @@ export const Header = ({ startLogout }) => (
     <h1>Expensify</h1>
     <ul>
       <li>
-        <NavLink exact to="/dashboard" activeClassName="is-active">
+        <NavLink to="/dashboard" activeClassName="is-active">
           Dashboard
         </NavLink>
       </li>
       <li>
         <NavLink to="/create" activeClassName="is-active">
           Add Expense
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/help" activeClassName="is-active">
-          Help
         </NavLink>
       </li>
     </ul>
--- src/components/HelpPage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const HelpPage = () => <div>help component</div>;
-
-export default HelpPage;
